98957fb8dd17af9b32f41235320cd30ec56703c4
Alec Ashford <ashford91@gmail.com>
2019-05-07 17:15:38 -0700

    added main function and python version

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is a script that attempts to guess a person's email address from their first name using common email patterns of corporate email addresses. Some use-cases for this include sales or marketing email campaigns or personal networking.
 
 The script uses the API found at verify-email.org, you will need a key. It also expects a headerless, two-column CSV of full names (e.g. "John Doe") and corporate email domains (e.g. "examplecompany.com") in that order. Place both in the main directory and name them as seen in the .gitignore (api_key.txt and data.csv, respectively).
+
+This script was developed using Python 3.7.
--- check_email.py
+++ check_email.py
@@ -3,64 +3,68 @@ import json
 import csv
 
 
-base_query = "https://app.verify-email.org/api/v1/{}/verify/{}"
-api_key = open("api_key.txt", "r").readline().strip()
+BASE_QUERY = "https://app.verify-email.org/api/v1/{}/verify/{}"
+API_KEY = open("api_key.txt", "r").readline().strip()
 
 successful_guess = []
 failed_to_guess = []
 needs_manual_processing = []
 
-with open('data.csv') as csv_file:
-	csv_reader = csv.reader(csv_file, delimiter=',')
-	for row in csv_reader:
-		full_name = row[0].lower().strip()
-		domain = row[1].lower()
+def main():
+	with open('data.csv') as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		for row in csv_reader:
+			full_name = row[0].lower().strip()
+			domain = row[1].lower()
+			
+			first_name = full_name.split(" ")[0]
+			last_name = full_name.split(" ")[-1]
+			
+			emails_to_try = [
+				"{}{}@{}".format(first_name, last_name, domain),
+				"{}{}@{}".format(last_name, first_name, domain),
+				"{}.{}@{}".format(first_name, last_name, domain),
+				"{}.{}@{}".format(last_name, first_name, domain),
+				"{}{}@{}".format(first_name[0], last_name, domain),
+				"{}{}@{}".format(first_name, last_name[0], domain),
+				"{}@{}".format(first_name, domain),
+				"{}@{}".format(last_name, domain),
+			]
 		
-		first_name = full_name.split(" ")[0]
-		last_name = full_name.split(" ")[-1]
+			confirmed_failure = True
+			
+			for email in emails_to_try:
+				r = requests.get(BASE_QUERY.format(API_KEY, email))
+				json_data = json.loads(r.text)
+				status_description = json_data["status_description"]
+				smtp_log = json_data["smtp_log"]
+				if status_description == "OK email":
+					successful_guess.append("{} valid email for {}".format(email, full_name))
+					confirmed_failure = False
+					break
+				elif status_description == "UNKNOWN email":
+					needs_manual_processing.append(full_name)
+					confirmed_failure = False
+					break
+				elif smtp_log != "MailboxDoesNotExist":
+					print("Unexpected error for {}, smtp_log: {}".format(email, smtp_log))
+					confirmed_failure = False
 		
-		emails_to_try = [
-			"{}{}@{}".format(first_name, last_name, domain),
-			"{}{}@{}".format(last_name, first_name, domain),
-			"{}.{}@{}".format(first_name, last_name, domain),
-			"{}.{}@{}".format(last_name, first_name, domain),
-			"{}{}@{}".format(first_name[0], last_name, domain),
-			"{}{}@{}".format(first_name, last_name[0], domain),
-			"{}@{}".format(first_name, domain),
-			"{}@{}".format(last_name, domain),
-		]
-	
-		confirmed_failure = True
+			if confirmed_failure:
+				failed_to_guess.append(full_name)
 		
-		for email in emails_to_try:
-			r = requests.get(base_query.format(api_key, email))
-			json_data = json.loads(r.text)
-			status_description = json_data["status_description"]
-			smtp_log = json_data["smtp_log"]
-			if status_description == "OK email":
-				successful_guess.append("{} valid email for {}".format(email, full_name))
-				confirmed_failure = False
-				break
-			elif status_description == "UNKNOWN email":
-				needs_manual_processing.append(full_name)
-				confirmed_failure = False
-				break
-			elif smtp_log != "MailboxDoesNotExist":
-				print("Unexpected error for {}, smtp_log: {}".format(email, smtp_log))
-				confirmed_failure = False
-	
-		if confirmed_failure:
-			failed_to_guess.append(full_name)
-	
-	print("Script complete! Successful guesses for these names:")
-	for line in successful_guess:
-		print(line)
-	
-	print("\nThese names need manual processing:")
-	
-	for name in needs_manual_processing:
-		print(name)
-	
-	print("\nScript failed to guess an email for these names:")
-	for name in failed_to_guess:
-		print(name)
+		print("Script complete! Successful guesses for these names:")
+		for line in successful_guess:
+			print(line)
+		
+		print("\nThese names need manual processing:")
+		
+		for name in needs_manual_processing:
+			print(name)
+		
+		print("\nScript failed to guess an email for these names:")
+		for name in failed_to_guess:
+			print(name)
+
+if __name__=="__main__":
+	main()
